f2c9d71cabc8aaf8dca0ae940513a22e117a6800
Ryan Andonian <ryanandonian@gmail.com>
2014-12-06 12:18:56 -0800

    Added a counter with logic to animate properly

--- Assets/Scripts/Controllers/FallenAllyController.cs
+++ Assets/Scripts/Controllers/FallenAllyController.cs
@@ -3,17 +3,33 @@ using System.Collections;
 
 public class FallenAllyController : MonoBehaviour {
     
+    public GameObject GO_TIMER;
+    private UI2DSpriteAnimation mTimerAnimation;
+
+    private const float kHealTime = 2.0f;
+    private float mHealStartTime;
+
     private ParticleSystem mParticleSystem;
 
 	// Use this for initialization
 	void Start () {
         mParticleSystem = GetComponentInChildren<ParticleSystem>();
         mParticleSystem.Stop();
+        GO_TIMER.SetActive(false);
+        mTimerAnimation = GO_TIMER.GetComponent<UI2DSpriteAnimation>();
+        mTimerAnimation.framesPerSecond = Mathf.RoundToInt(mTimerAnimation.frames.Length / kHealTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        // If we're doing timing
+        if (GO_TIMER.activeSelf)
+        {
+            if (mHealStartTime + kHealTime < Time.time)
+            {
+                // Do the poof or whatever. Add score
+            }
+        }
 	}
 
     void OnTriggerEnter(Collider other)
@@ -21,6 +37,10 @@ public class FallenAllyController : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             mParticleSystem.Play();
+            GO_TIMER.SetActive(true);
+            mTimerAnimation.ResetToBeginning();
+            mTimerAnimation.Play();
+            mHealStartTime = Time.time;
         }
     }
 
@@ -29,6 +49,9 @@ public class FallenAllyController : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             mParticleSystem.Stop();
+            GO_TIMER.SetActive(false);
+            mHealStartTime = float.MaxValue;
+            mTimerAnimation.Pause();
         }
     }
 }
