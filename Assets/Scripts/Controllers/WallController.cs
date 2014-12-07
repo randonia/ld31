c4a17efa8c570ee44155948421a43bdf0d503054
Ryan Andonian <ryanandonian@gmail.com>
2014-12-06 18:56:56 -0800

    Added world boundaries

---
+++ Assets/Scripts/Controllers/WallController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, GetComponent<BoxCollider>().size);
+    }
+}
